25a9ab43d0ff65f02152ddfa90c07bc7d348d754
Max Ismagilov <mismagilov25@students.polytechnic.org>
2024-02-27 09:29:20 -0800

    Add node and unordered list

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # AdvancedTopicsDataStructures
+
+This is a collection of Data Structures
+
+It currently contains:
+ - Stack
+ - Queue
+ - Deque
+ - Node
--- code/atds.py
+++ code/atds.py
@@ -94,5 +94,136 @@ class Deque:
     def is_empty(self):
         return len(self.arr) == 0
 
+class Node:
+    def __init__(self) -> None:
+        self.data = None
+        self.next = None
+        
+    def __init__(self, data, next) -> None:
+        self.data = None
+        self.next = None
+        
+    def get_data(self):
+        return self.data
+    
+    def get_next(self):
+        return self.next
+    
+    def set_data(self, value):
+        self.data = value
+        
+    def set_next(self, value):
+        self.next = value
+        
+    def __repr__(self) -> str:
+        return f"Node(data: {self.data}, next:{self.next})"
+        
+class UnorderedList:
+    def __init__(self) -> None:
+        self.head = None
+    
+    def add(self, item):
+        head_val = self.head
+        self.head = Node(item, head_val)
+    
+    def remove(self, item):
+        """
+        removes an item
 
-
+        """
+        n = self.head
+        if (self.head == None):
+            return
+        if (self.head.get_data() == item):
+            self.head = self.head.get_next()
+            return
+        while (n.get_next() != None and n.get_next().get_data() != item):
+            n = n.get_next()
+        if (n.get_next() != None):
+            n = n.set_next(n.get_next().get_next())
+            return
+        return
+    
+    
+    def is_empty(self):
+        return self.head == None
+    
+    def length(self):
+        count = 0
+        n = self.head()
+        while (n != None):
+            count += 1
+            n = n.get_next()
+        return count
+    
+    def append(self, item):
+        if self.head == None:
+            self.add(item)
+            return
+        n = self.head()
+        while (n.get_next() == None):
+            n = n.get_next()
+        n.set_next(Node(item, None))
+        
+    def index(self, item):
+        count = -1
+        n = self.head()
+        while (n != None):
+            count += 1
+            if (n.get_data() == item):
+                return count
+            n = n.get_next()
+        return -1
+        
+    def search(self, item):
+        return self.search(item) != -1
+    
+    def insert(self, pos, item):
+        n = self.head()
+        for i in range(0, pos):
+            n = n.get_next()
+        next = n
+        n = Node(item, next)
+        
+    def pop(self):
+        if self.head == None:
+            return None
+        if self.head.get_next() == None:
+            val = self.head.get_data()
+        n = self.head
+        while (n.get_next().get_next() != None):
+            n = n.get_next()
+        
+        val = n.get_next()
+        n.set_next(None)
+        return val 
+    
+    def pop(self, pos):
+        if pos == 0:
+            self.head = self.head.get_next()
+            return
+        n = self.head()
+        for i in range(0, pos-1):
+            n = n.get_next()
+        val = n.get_next().get_data()
+        n.set_next(n.get_next().get_next())
+        return val
+        
+    def __repr__(self): # literally just Mr. White's method
+        """Creates a representation of the list suitable for printing,
+        debugging.
+        """
+        result = "UnorderedList["
+        next_node = self.head
+        while next_node != None:
+            result += str(next_node.get_data()) + ","
+            next_node = next_node.get_next()
+        result = result + "]"
+        return result
+        
+        
+            
+    
+            
+            
+            
